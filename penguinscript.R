fe118c44a83a19f24a35eb3070469c557b650f16
Kathleen Owen <kathleenowen@MacBook-Air-280.local>
2024-09-25 14:35:05 +0100

    linear regression

--- penguinscript.R
+++ penguinscript.R
@@ -5,6 +5,12 @@ library(tidyverse)
 
 #load penguin data 
 
-penguin_data <- read.table("./data/penguin_data.txt")
+penguin_data <- read_table("./data/penguin_data.txt")
 
 plot(penguin_data)
+
+#linear regression 
+
+model1 <- lm(body_mass_g ~ flipper_length_mm, data = penguin_data)
+summary(model1)
+
