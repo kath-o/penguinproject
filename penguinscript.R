be302c29d8d214bf98e4e332fd7629011c60af9a
Kathleen Owen <kathleenowen@MacBook-Air-280.local>
2024-09-25 14:35:44 +0100

    plot 2

--- penguinscript.R
+++ penguinscript.R
@@ -14,3 +14,8 @@ plot(penguin_data)
 model1 <- lm(body_mass_g ~ flipper_length_mm, data = penguin_data)
 summary(model1)
 
+#plot
+
+ggplot(penguin_data, aes(x = flipper_length_mm, y = body_mass_g, colour = species)) +
+  geom_point() +
+  stat_smooth(method = "lm")  
